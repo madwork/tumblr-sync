6ebf1d4d33d6b34110a5b996d708bfe86821cd3c
Vincent Durand <vincent.durand@madwork.org>
2012-10-30 22:21:57 +0100

    First working version

--- lib/tumblr-sync.rb
+++ lib/tumblr-sync.rb
@@ -1,25 +1,34 @@
 require 'mechanize'
 
 module TumblrSync
+  MAX = 50
+  
   class Site
-    attr_reader :agent, :host
+    attr_reader :host
     
     def initialize(host)
       @host = host
       @endpoint = "http://#{host}/api/read?type=photo"
-      @agent = Mechanize.new
     end
     
-    def images(start, number = 50)
-      response = @agent.get("#@endpoint&start=#{start}&num=#{number}")
-      doc = Nokogiri::XML.parse(response.body)
+    def images(start, number = MAX)
+      http = Mechanize.new
+      http.get("#@endpoint&start=#{start}&num=#{number}")
+      doc = Nokogiri::XML.parse(http.page.body)
       doc.xpath("//posts/post/photo-url[@max-width='1280']").map(&:text)
     end
     
     def total
-      response = @agent.get(@endpoint)
-      doc = Nokogiri::XML.parse(response.body)
-      doc.xpath("//posts/@total").text.to_i
+      @total ||= begin
+        http = Mechanize.new
+        http.get(@endpoint)
+        doc = Nokogiri::XML.parse(http.page.body)
+        doc.xpath("//posts/@total").text.to_i
+      end
+    end
+    
+    def times(&block)
+      (total.to_f / MAX + 0.5).round.times(&block)
     end
   end
 end
