97d4ed2547079eb1998bfbd99e1089ca453ee395
Vincent Durand <vincent.durand@madwork.org>
2012-11-02 11:19:53 +0100

    Add mechaconf block parameter

--- lib/tumblr-sync.rb
+++ lib/tumblr-sync.rb
@@ -6,13 +6,18 @@ module TumblrSync
   class Site
     attr_reader :host
     
-    def initialize(host)
+    # Example:
+    # 
+    # t = TumblrSync::Site.new("citieslight.tumblr.com") { |a| a.user_agent_alias = 'Mac Safari' }
+    #
+    def initialize(host, &block)
       @host = host
       @endpoint = "http://#{host}/api/read?type=photo"
+      @mechaconf = block
     end
     
     def images(start, number = MAX)
-      http = Mechanize.new
+      http = Mechanize.new(&@mechaconf)
       http.get("#@endpoint&start=#{start}&num=#{number}")
       doc = Nokogiri::XML.parse(http.page.body)
       doc.xpath("//posts/post/photo-url[@max-width='1280']").map(&:text)
@@ -20,7 +25,7 @@ module TumblrSync
     
     def total
       @total ||= begin
-        http = Mechanize.new
+        http = Mechanize.new(&@mechaconf)
         http.get(@endpoint)
         doc = Nokogiri::XML.parse(http.page.body)
         doc.xpath("//posts/@total").text.to_i
